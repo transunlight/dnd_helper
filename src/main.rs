e181648ac7b016ada1fee3fceefe3b61954d8dfc
transunlight <thetruthshaper@gmail.com>
2023-12-21 19:29:45 +0530

    Extract query logic to get_spell_info
    - use Response::error_for_status to improve code readability

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 use std::io::{self, Write};
 
+use reqwest::blocking::{Client, Response};
 use scraper::Selector;
 
 fn main() {
     let info_selector = Selector::parse("#page-content > *").unwrap();
-    let client = reqwest::blocking::Client::new();
+    let client = Client::new();
 
     while let Some(spell_name) = get_spell_name() {
         println!("Searching for {spell_name}...");
 
-        let url = format!("http://dnd5e.wikidot.com/spell:{spell_name}");
-
-        let response = match client.get(&url).send() {
+        let response = match get_spell_info(&client, &spell_name) {
             Ok(res) => res,
             Err(err) => {
                 eprintln!("{}", err);
@@ -19,12 +18,7 @@ fn main() {
             }
         };
 
-        if response.status() != reqwest::StatusCode::OK {
-            println!("Could not find {spell_name}.");
-            continue;
-        }
-
-        let document = scraper::Html::parse_document(&(response.text().unwrap()));
+        let document = scraper::Html::parse_document(response.text().unwrap().as_str());
 
         let raw_spell_info: Vec<_> = document
             .select(&info_selector)
@@ -46,11 +40,21 @@ fn get_spell_name() -> Option<String> {
     io::stdout().flush().unwrap();
 
     io::stdin().read_line(&mut name).unwrap();
-    name = name.trim().replace(' ', "-");
+    name = name.trim().to_string();
 
     if name.is_empty() {
         None
     } else {
         Some(name)
     }
+}
+
+fn get_spell_info(client: &Client, spell_name: &str) -> Result<Response, reqwest::Error> {
+    let spell_name = spell_name.replace(' ', "-");
+    let url = format!("http://dnd5e.wikidot.com/spell:{spell_name}");
+
+    client
+        .get(url)
+        .send()
+        .and_then(|res| res.error_for_status())
 }
