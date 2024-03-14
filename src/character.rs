a300b9d2dee50df29d88c2aea797a1b9aa315d26
transunlight <thetruthshaper@gmail.com>
2024-03-15 00:55:00 +0530

    Add attributes to Character
    - Add enum Attribute
    - Add struct AttrVal

--- src/character.rs
+++ src/character.rs
@@ -1,6 +1,9 @@
+use enum_map::EnumMap;
+
 #[derive(Debug)]
 pub struct Character {
     pub name: String,
+    pub attributes: EnumMap<Attribute, AttrVal>,
     pub prof_bonus: i8,
 }
 
@@ -8,7 +11,41 @@ impl Character {
     pub fn create_altaea() -> Self {
         Self {
             name: "Altaea".to_string(),
+            attributes: EnumMap::default(),
             prof_bonus: 2,
         }
     }
 }
+
+mod attribute {
+    use enum_map::Enum;
+    use strum::Display;
+
+    #[derive(Debug, Enum, Display)]
+    pub enum Attribute {
+        Strength,
+        Dexterity,
+        Constitution,
+        Intelligence,
+        Wisdom,
+        Charisma,
+    }
+
+    #[derive(Debug, Default)]
+    pub struct AttrVal {
+        pub score: i8,
+    }
+
+    impl AttrVal {
+        pub fn modifier(&self) -> i8 {
+            (self.score - 10) / 2
+        }
+    }
+
+    impl From<i8> for AttrVal {
+        fn from(score: i8) -> Self {
+            Self { score }
+        }
+    }
+}
+use attribute::{AttrVal, Attribute};
